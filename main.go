fa71eefe80ff780d61926c23c9f75f82a760b216
Michael Brown <micha3lbrown@gmail.com>
2018-05-29 08:56:02 -0400

    Break KafkaClient into own file

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/segmentio/kafka-go"
-)
-
 func main() {
-	consumer := initKafka()
-	consumeKafka(consumer)
-}
-
-func initKafka() *kafka.Reader {
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:   []string{"localhost:9092"},
-		Topic:     "test",
-		Partition: 0,
-		MinBytes:  10e3, // 10KB
-		MaxBytes:  10e6, // 10MB
-	})
-	return r
-}
-
-func consumeKafka(r *kafka.Reader) {
-	for {
-		m, err := r.ReadMessage(context.Background())
-		if err != nil {
-			break
-		}
-		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
-	}
-
-	r.Close()
+	KafkaClient()
 }
